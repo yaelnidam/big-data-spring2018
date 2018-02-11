e400bc41bd169a0526d64a631d6cc59210c5288c
yaelnidam <yaeln@mit.edu>
2018-02-10 19:53:57 -0500

    edit pset1

--- week-02/submission/pset1.py
+++ week-02/submission/pset1.py
@@ -1,8 +1,6 @@
-print("starting pset1")
-print(" ")
+#starting pset1
 
-
-print("A.Lists")
+#A.Lists
 l=["girls","just","wanna","have fun"] #A.1 Create a list containing any 4 strings.
 print(l[2]) #A.2 Print the 3rd item in the list
 print(l[0:2]) #A.3 Print the 1st and 2nd item in the list using [:] index slicing
@@ -11,10 +9,9 @@ print(l)
 print(len(l), "items in list") #A.5 Get the list length and print it.
 l[4]="new" #A.6 Replace the last item in the list with the string “new” and print
 print(l)
-print(" ")
 
 
-print("B.Strings")
+#B.Strings
 sentence_words=['I','am','learning','Python','to','munge','large','datasets','and','visualize','them']
 print(' '.join(sentence_words)) #B.1 Convert the list into a normal sentence with join(), then print
 sentence_words.reverse() #B.2 Reverse the order of this list using the .reverse() method, then print.
@@ -25,20 +22,19 @@ print("sorted()",sorted(sentence_words))#B.4.1 Perform the same operation using 
 print(".sort() is a method, it acts in place and on the object that called, sorted() is a function, it creates a new list and act upon a variable inserted in its ()")#B.4.2Provide a brief description of how the sorted() function differs from the .sort() method.
 sws=sorted(sentence_words, key=lambda x:x.lower())#B.5 case-insensitive alphabetical sort.
 print(sws)
-print(" ")
 
 
-print("C. Random Function")
+#C. Random Function
 from random import randint
 def my_random(high,low=0):#C random function returning an integer between a low and a high number supplied by the user, but that can be called with the low number optional (default to 0).
     return randint(low,high)
 assert(0 <= my_random(100) <= 100)
 assert(50 <= my_random(100, low = 50) <= 100)
 print(my_random(150,-50)) #print random number within bounds -50<x<150
-print(" ")
 
 
-print("E. Password Validation Function")
+
+#E. Password Validation Function
 password="Fun_2018" #insert any password to be checked
 
 def checker(x): #function to check password for compliance with rules
@@ -68,9 +64,9 @@ def checker(x): #function to check password for compliance with rules
         print ("Your password doesn't follow the rules, try again")
         return "error"
 checker(password)
-print(" ")
 
-print("F. Exponentiation Function")
+
+#F. Exponentiation Function"
 def exp(x,y):
     total=1
     i=1
@@ -78,10 +74,9 @@ def exp(x,y):
         total=total*x
         i=i+1
     return total
-print(" ")
 
 
-print("G. Extra Credit: Min and Max Functions")
+#G. Extra Credit: Min and Max Functions"
 
 def minimum(x): #min definition
     min_=x[0]
