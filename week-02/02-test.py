ccce577c2e0e6705bf9730406bf3416c58bb75e9
yaelnidam <yaeln@mit.edu>
2018-02-20 10:44:37 -0500

    update from class

---
+++ week-02/02-test.py
@@ -0,0 +1,19 @@
+from math import pi
+print(pi)
+import numpy as np
+import panda as pd
+h="Happy times"
+t="all around us"
+k=h.find("times")
+print(k)
+
+#f string
+msg=f"we enjoy {h} when our loved once are {t}"
+msg2="we enjoy {} when our loved once are {}".format(h,t)
+print(msg2)
+
+x=1
+y=1.0
+
+print(x==y)
+print(x is y)
